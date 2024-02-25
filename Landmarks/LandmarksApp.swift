a475a0c3b4603fbfb81d38053be5b37a496bd24f
FranckMight1555 <francktonlebruno@gmail.com>
2024-02-25 10:46:54 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Franck Tonle on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
